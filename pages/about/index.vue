df9e71c23d2cfa6cc47711549ee97d210f330882
lbavendanos <lbavendanos@gmail.com>
2018-06-23 13:10:57 -0500

    Install sass

---
+++ pages/about/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1>Hola About</h1>
+</template>
+
+<script>
+    export default {
+        head() {
+            return {
+                title: 'about'
+            }
+        }
+    }
+</script>
