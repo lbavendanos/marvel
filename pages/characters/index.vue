7c20af2160cffe789160a02b6b2ee1249583b1cf
lbavendanos <lbavendanos@gmail.com>
2018-12-21 01:23:51 -0500

    Add banner to the Character section

---
+++ pages/characters/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <v-img src="https://terrigen-cdn-dev.marvel.com/content/prod/2x/characters_art_mas_dsk_01.jpg">
+      <v-container fill-height class="white--text">
+        <v-layout align-center>
+          <v-flex>
+            <h3 class="display-3 text-xs-center">CHARACTERS</h3>
+            <h4
+              class="text-xs-center subheading"
+            >Dive into the dazzling domain of all the classic characters you love - and those you’ll soon discover!</h4>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-img>
+  </section>
+</template>
--- pages/comics/index.vue
+++ pages/comics/index.vue
@@ -1,5 +1,20 @@
 <template>
-  <div></div>
+  <section>
+    <v-img src="https://terrigen-cdn-dev.marvel.com/content/prod/2x/characters_art_mas_dsk_01.jpg">
+      <v-container fill-height class="white--text">
+        <v-layout align-center>
+          <v-flex>
+            <h3 class="display-3">Welcome to the site</h3>
+            <span
+              class="subheading"
+            >Lorem ipsum dolor sit amet, pri veniam forensibus id. Vis maluisset molestiae id, ad semper lobortis cum. At impetus detraxit incorrupte usu, repudiare assueverit ex eum, ne nam essent vocent admodum.</span>
+            <v-divider class="my-3"></v-divider>
+            <div class="title mb-3">Check out our newest features!</div>
+          </v-flex>
+        </v-layout>
+      </v-container>
+    </v-img>
+  </section>
 </template>
 
 <script>
