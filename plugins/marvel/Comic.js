a785c0e469c60ba57132a94a63f410b2cbca6fdf
lbavendanos <lbavendanos@gmail.com>
2019-01-02 14:51:35 -0500

    Fix url comics

--- plugins/marvel/Comic.js
+++ plugins/marvel/Comic.js
@@ -1,7 +1,7 @@
 import Base from '@/plugins/marvel/Base'
 
 export default class Comic extends Base {
-  static BASE_URL = `${process.env.MARVEL_URL}/Comics`
+  static BASE_URL = `${process.env.MARVEL_URL}/comics`
 
   constructor(context) {
     super(context)
