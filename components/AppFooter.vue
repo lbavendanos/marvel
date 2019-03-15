50a687e8ce8cec642e3990d36cd57e3098c6fa04
Luis Avendaño <luis.avendano@flexit.net>
2019-03-15 12:02:13 -0500

    Add social info

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,26 +1,27 @@
 <template>
-  <v-footer 
-    class="red" 
+  <v-footer
+    class="red"
     height="auto">
     <v-container class="py-1">
-      <v-layout 
-        row 
-        wrap 
-        justify-space-between 
+      <v-layout
+        row
+        wrap
+        justify-space-between
         align-center>
         <div>
           <h6 class="caption white--text">LBAVENDANOS &copy; {{ new Date().getFullYear() }}</h6>
         </div>
         <div>
-          <v-btn 
-            v-for="icon in icons" 
-            :key="icon" 
-            dark 
-            small 
-            color="white" 
-            icon 
+          <v-btn
+            v-for="(value, key, index) in social"
+            :key="index"
+            :href="value.to"
+            dark
+            small
+            color="white"
+            icon
             class="mx-1 my-1">
-            <v-icon color="red">{{ icon }}</v-icon>
+            <v-icon color="red">{{ value.icon }}</v-icon>
           </v-btn>
         </div>
       </v-layout>
@@ -32,13 +33,19 @@
 export default {
   data() {
     return {
-      icons: [
-        'fa fa-github',
-        'fa fa-facebook-official',
+      social: [
+        { icon: 'fa fa-github', to: 'https://lbavendanos.github.io' },
+        {
+          icon: 'fa fa-facebook-official',
+          to: 'https://www.facebook.com/lbavendanos'
+        },
         //   'fa fa-twitter-square',
         //   'fa fa-google-plus-official',
         //   'fa fa-linkedin-square',
-        'fa fa-instagram'
+        {
+          icon: 'fa fa-instagram',
+          to: 'https://www.instagram.com/lbrayan.avendano'
+        }
       ]
     }
   }
